18c95d36cff51f430f62f2d7a4643c224d2c2144
NAGULESH N <143864776+NaguleshN@users.noreply.github.com>
2024-06-12 21:19:23 +0530

    Delete Shell directory

--- Shell/append.sh
+++
@@ -1,7 +0,0 @@
-echo "Appending a file"
-echo "Enter content of the file"
-cat > abc
-echo "Enter content to append in the file"
-cat >> abc
-echo "Appended file"
-cat abc
--- Shell/str_search.sh
+++
@@ -1,9 +0,0 @@
-echo "Enter the parent string : "
-read STR
-echo "Enter the substring to search : "
-read SUB
-if [[ "$STR" == *"$SUB"* ]]; then
-  echo "Match is found."
-else
-  echo "Match not found."
-fi
